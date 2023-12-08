f4283279e2f18944b5081f0dd63d0d0f900fc5aa
ClydeArrowny <francisco_ing@outlook.com>
2023-12-08 20:21:09 -0300

    Quartz sync: Dec 8, 2023, 8:21 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,32 +1,25 @@
-// Import necessary types and styles
-import { QuartzComponentConstructor, QuartzComponentProps } from "./types";
-import style from "./styles/footer.scss";
-import { version } from "../../package.json";
-import * as Component from "./quartz/components";
-import Graph, { GraphOptions } from "./quartz/components";
+import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import style from "./styles/footer.scss"
+import { version } from "../../package.json"
 
 interface Options {
-  links: Record<string, string>;
+  links: Record<string, string>
 }
 
-export default ((opts?: Options & GraphOptions) => {
+export default ((opts?: Options) => {
   function Footer({ displayClass }: QuartzComponentProps) {
-    const year = new Date().getFullYear();
-    const links = opts?.links ?? [];
-
-    const graphComponent = Graph(opts);
-
+    const year = new Date().getFullYear()
+    const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
           Clyde Arrowny, © {year}
         </p>
-        {graphComponent} {Component.Graph()}
       </footer>
-    );
+    )
   }
 
-  Footer.css = style;
-  return Footer;
-}) satisfies QuartzComponentConstructor;
+  Footer.css = style
+  return Footer
+}) satisfies QuartzComponentConstructor
